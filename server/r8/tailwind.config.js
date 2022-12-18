2743cb5963c46d292a1ad7659945a53ca4d02c8f
Daniel Gonzalez <daniel.gonzalez021@gmail.com>
2022-12-18 13:54:02 -0500

    Added landing page and updated login

--- server/r8/tailwind.config.js
+++ server/r8/tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      'dark-blue': '#3B4662'
+    },
     extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      },
         fontFamily: {
             sans: ["STIX Two Text", "serif"],
         }
@@ -11,5 +20,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ]
 }
